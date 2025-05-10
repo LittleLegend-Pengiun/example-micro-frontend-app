1ac4bece3e4c8a7a788888009d7c098fc9031b07
Hoang Anh <hoanganhleboy@gmail.com>
2025-05-10 20:40:27 +0700

    Update

--- container/src/App.jsx
+++ container/src/App.jsx
@@ -6,11 +6,11 @@ const App = () => {
     <div>
       <h1>Container App</h1>
       <div style={{ border: "1px dashed #ccc", padding: "10px" }}>
-        <EmbededMicroFrontEnd
+        {/* <EmbededMicroFrontEnd
           remoteUrl="http://localhost:3001/remoteEntry.js"
           scope="remote"
           module="./mount1"
-        />
+        /> */}
       </div>
     </div>
   );

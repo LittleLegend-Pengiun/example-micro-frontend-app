ed34b59b90ebe8f4c5f29ca124520e80adca368d
Hoang Anh <hoanganhleboy@gmail.com>
2025-05-12 20:48:30 +0700

    Update

--- template/bootstrap.jsx
+++ template/bootstrap.jsx
@@ -2,5 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./src/App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+export function mount(el) {
+  const root = createRoot(el);
+  root.render(<App />);
+}
+
+// Auto-render if run as standalone (e.g. localhost:3001)
+if (process.env.NODE_ENV === "development") {
+  const standaloneRoot = document.getElementById("root");
+  if (standaloneRoot) {
+    mount(standaloneRoot);
+  }
+}

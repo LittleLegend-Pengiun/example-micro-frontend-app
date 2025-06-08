860fdedb26514fc65376fad8cc59c0eee4a76703
hdthinh1012 <hdthinh01@gmail.com>
2025-06-09 01:18:07 +0700

    clean

--- cli/angular_app_1/projects/microfrontend-loader/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of microfrontend-loader
- */
-
-export * from './lib/services/remote-loader.service';
-export * from './lib/components/embedded-microfrontend/embedded-microfrontend.component';
-export * from './lib/microfrontend-loader.module';

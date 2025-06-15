19c5d3c64ccf34c49673494038b5f1dd7b7dd119
hdthinh1012 <hdthinh01@gmail.com>
2025-06-15 13:03:12 +0700

    feat: error of angular import angular left

---
+++ cli/angular_app_1/projects/microfrontend-loader/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of microfrontend-loader
+ */
+
+export * from './lib/services/remote-loader.service';
+export * from './lib/components/embedded-microfrontend/embedded-microfrontend.component';
+export * from './lib/microfrontend-loader.module';

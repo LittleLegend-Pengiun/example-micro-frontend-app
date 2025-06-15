a56d826c070b8250ed981764dca81587466b50ca
hdthinh1012 <hdthinh01@gmail.com>
2025-06-15 13:01:57 +0700

    feat: run as standalone angular mfe

---
+++ template/angular/src/bootstrap.ts
@@ -0,0 +1,20 @@
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModule } from './app/app.module';
+
+platformBrowser().bootstrapModule(AppModule, {
+  ngZoneEventCoalescing: true,
+})
+  .catch(err => console.error(err));
+
+export function mount(el: HTMLElement | Element) {
+  // Clear the element first (optional, to avoid duplicates)
+  el.innerHTML = '';
+  // Create the Angular custom element and append it to the provided container
+  const angularElement = document.createElement('angular-element');
+  el.appendChild(angularElement);
+}
+
+const standaloneRoot = document.querySelector("app-root");
+if (standaloneRoot) {
+  mount(standaloneRoot);
+}

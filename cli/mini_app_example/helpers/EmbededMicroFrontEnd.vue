1f659de03a4fbf7e229d9b545fb971b40f2fc19c
hdthinh1012 <hdthinh01@gmail.com>
2025-07-01 22:38:05 +0700

    feat: support multiple framework options

---
+++ cli/mini_app_example/helpers/EmbededMicroFrontEnd.vue
@@ -0,0 +1,21 @@
+<template>
+  <div ref="remoteContainer"></div>
+</template>
+
+<script>
+import { loadRemote } from "./loadRemoteModule";
+
+export default {
+  props: ['remoteUrl', 'scope', 'module'],
+  async mounted() {
+    loadRemote(this.$props.remoteUrl, this.$props.scope, this.$props.module)
+    .then((remote) => {
+      console.log("loadRemote then moute::", remote);
+      remote.mount(this.$refs.remoteContainer);
+    })
+    .catch((err) => {
+      console.error("Failed to load remote:", err);
+    });
+  },
+};
+</script>
